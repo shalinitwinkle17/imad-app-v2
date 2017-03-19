27929653b43ac121b4b2209c53f2e4fc21870125
shalinitwinkle17 <shalinitwinkle17@gmail.com>
2017-03-19 20:13:07 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -57,7 +57,7 @@ function createTemplate (data) {
 }
 
 app.get('/article/:articleName', function (req, res){
-    pool.query("SELECT * FROM article WHERE title = '" + req.params.articleName + "'", function (req, res) {
+    pool.query("SELECT * FROM article WHERE title = '" + req.params.articleName + "'", function (err, result) {
         if(err) {
             res.status(500).send(err.toString());
         }
