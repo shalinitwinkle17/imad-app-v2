03bce7fb0618b07ee93442526ef049df105ebb94
shalinitwinkle17 <shalinitwinkle17@gmail.com>
2017-03-19 21:02:50 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ var app = express();
 app.use(morgan('combined'));
 var pool = new Pool(config);
 app.get('/test-db',function(req, res){
-    pool.query('SELECT *FROM test',function(err, result){
+    pool.query('SELECT * ROM test',function(err, result){
         if(err){
             res.status(500).send(err.toString());
         } else {
