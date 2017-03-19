c6108b39ad249f30c2367b9b176ca71d4f7cfe48
shalinitwinkle17 <shalinitwinkle17@gmail.com>
2017-03-19 20:05:12 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
 var Pool = require('pg').Pool;
-
 var config = {
     user: 'shalinitwinkle17',
     database: 'shalinitwinkle17',
@@ -22,9 +21,56 @@ app.get('/test-db',function(req, res){
         }
     });
 })
-
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+
+function createTemplate (data) {
+    var HTMLtemplate = `
+    <html>
+        <head>
+            <title>
+                ${data.title}
+            </title>
+            <meta name="viewport" content="width=device-width, initial-scale=1"/>
+            <link href="/ui/style.css" rel="stylesheet" />
+        </head>
+        <body>
+        <div class="box">
+            <div>
+                <a href="/">Home</a>
+            </div>
+            <h2>
+                $(data.heading)
+            </h2>
+            <div>
+                $(data.date)
+            </div>
+            <div>
+                $(data.content)
+            </div>
+        </div>
+    </body>
+    </html>
+`
+    
+}
+
+app.get('/article/:articleName', function (req, res){
+    pool,query("SELECT * FROM article WHERE title = '" + req.params.articleName + "'", function (req, res) {
+        if(err) {
+            res,status(500).send(err.toString());
+        }
+        else {
+            if (result.rows.length === 0) {
+                res.status(404).send('Article not found');
+            }
+            else{
+                var aticleData = result.rows[0];
+                res.send(createTemplate(create));          
+            }
+        }
+    });
 });
 
 app.get('/article-one', function (req, res) {
