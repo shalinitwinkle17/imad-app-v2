347d2505494200fcd3ce818fe02431df7082a62c
shalinitwinkle17 <shalinitwinkle17@gmail.com>
2017-03-19 18:50:14 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var config = {
     database: 'shalinitwinkle17',
     host: 'db.imad.hasura-app.io',
     port: '5432',
-    password: process.env.DB_PASSWORD
+    password:'db-shalinitwinkle17-67794'
 };
 var app = express();
 app.use(morgan('combined'));
