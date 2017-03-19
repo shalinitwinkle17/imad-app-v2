60b749e410ea7dce30eb66dc6390f8e5b7353a19
shalinitwinkle17 <shalinitwinkle17@gmail.com>
2017-03-19 18:21:20 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -17,8 +17,8 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
-app.get('/ui/madi.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+app.get('/ui/yr.jpg', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'yr.jpg'));
 });
 
 
